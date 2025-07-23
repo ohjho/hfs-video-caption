4fa18d95b7adc6996b5798ae7267982efe59a44c
John Ho <jho.yvr+git@gmail.com>
2025-07-23 16:51:11 -0400

    added function to read fps

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-import spaces
+import spaces, ffmpeg, os
 import gradio as gr
 from transformers import Qwen2_5_VLForConditionalGeneration, AutoProcessor
 from qwen_vl_utils import process_vision_info
+from loguru import logger
+
+logger.remove()
+logger.add(
+    sys.stderr,
+    format="<d>{time:YYYY-MM-DD ddd HH:mm:ss}</d> | <lvl>{level}</lvl> | <lvl>{message}</lvl>",
+)
 
 # --- Installing Flash Attention for ZeroGPU is special --- #
 import subprocess
@@ -14,6 +21,20 @@ subprocess.run(
 # --- now we got Flash Attention ---#
 
 # The model is trained on 8.0 FPS which we recommend for optimal inference
+
+
+def get_fps_ffmpeg(video_path: str):
+    probe = ffmpeg.probe(video_path)
+    # Find the first video stream
+    video_stream = next(
+        (stream for stream in probe["streams"] if stream["codec_type"] == "video"), None
+    )
+    if video_stream is None:
+        raise ValueError("No video stream found")
+    # Frame rate is given as a string fraction, e.g., '30000/1001'
+    r_frame_rate = video_stream["r_frame_rate"]
+    num, denom = map(int, r_frame_rate.split("/"))
+    return num / denom
 
 
 @spaces.GPU(duration=30)
@@ -40,10 +61,13 @@ def load_model(
 
 
 @spaces.GPU(duration=120)
-def inference(video_path: str):
+def inference(
+    video_path: str, prompt: str = "Describe the camera motion in this video."
+):
     # default processor
     processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct")
-
+    fps = get_fps_ffmpeg(video_path)
+    logger.info(f"{os.path.basename(video_path)} FPS: {fps}")
     messages = [
         {
             "role": "user",
@@ -51,9 +75,9 @@ def inference(video_path: str):
                 {
                     "type": "video",
                     "video": video_path,
-                    "fps": 8.0,
+                    "fps": fps,
                 },
-                {"type": "text", "text": "Describe the camera motion in this video."},
+                {"type": "text", "text": prompt},
             ],
         }
     ]
